7f1d2779daf4fc744f28dcb3140992c2072b05eb
sond-xx9 <sondwe8@gmail.com>
2026-01-15 00:44:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+io.on("connection", socket => {
+
+  socket.on("join_text", ({ username, room }) => {
+    socket.join(room);
+    socket.username = username;
+    socket.room = room;
   });
+
+  socket.on("text_message", msg => {
+    io.to(socket.room).emit("text_message", {
+      user: socket.username,
+      text: msg
+    });
+  });
+
+  socket.on("join_voice", room => {
+    socket.join(room);
+    socket.to(room).emit("user-joined");
+  });
+
+  socket.on("offer", data => socket.to(data.room).emit("offer", data));
+  socket.on("answer", data => socket.to(data.room).emit("answer", data));
+  socket.on("ice", data => socket.to(data.room).emit("ice", data));
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT);
+server.listen(process.env.PORT || 3000);
