530ea0a43f8931641a3f9419bc8860a792070b8f
sond-xx9 <sondwe8@gmail.com>
2026-01-15 00:46:18 +0300

    Refactor chat and voice functionality in script.js

--- public/script.js
+++ public/script.js
@@ -1,19 +1,58 @@
 const socket = io();
-const form = document.getElementById("form");
-const input = document.getElementById("message");
-const messages = document.getElementById("messages");
+let pc;
+let stream;
+let voiceRoom;
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (input.value.trim()) {
-    socket.emit("chat message", input.value);
-    input.value = "";
-  }
+function joinChat() {
+  socket.emit("join_text", {
+    username: chatName.value,
+    room: chatRoom.value
+  });
+}
+
+function send() {
+  socket.emit("text_message", msg.value);
+  msg.value = "";
+}
+
+socket.on("text_message", data => {
+  messages.innerHTML += `<div><b>${data.user}:</b> ${data.text}</div>`;
 });
 
-socket.on("chat message", (msg) => {
-  const li = document.createElement("li");
-  li.textContent = msg;
-  messages.appendChild(li);
-  messages.scrollTop = messages.scrollHeight;
+async function joinVoice() {
+  voiceRoom = document.getElementById("voiceRoom").value;
+
+  stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  pc = new RTCPeerConnection();
+
+  stream.getTracks().forEach(t => pc.addTrack(t, stream));
+
+  pc.onicecandidate = e => {
+    if (e.candidate)
+      socket.emit("ice", { room: voiceRoom, candidate: e.candidate });
+  };
+
+  pc.ontrack = e => {
+    const audio = document.createElement("audio");
+    audio.srcObject = e.streams[0];
+    audio.autoplay = true;
+  };
+
+  socket.emit("join_voice", voiceRoom);
+}
+
+socket.on("user-joined", async () => {
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  socket.emit("offer", { room: voiceRoom, offer });
 });
+
+socket.on("offer", async data => {
+  await pc.setRemoteDescription(data.offer);
+  const answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
+  socket.emit("answer", { room: voiceRoom, answer });
+});
+
+socket.on("answer", data => pc.setRemoteDescription(data.answer));
+socket.on("ice", data => pc.addIceCandidate(data.candidate));
