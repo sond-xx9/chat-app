043d501d36fb2a3406029c09d31fc6463451af72
sond-xx9 <sondwe8@gmail.com>
2026-01-14 23:29:54 +0300

    Refactor connection logs and comments in server.js

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// تقديم ملفات public
+app.use(express.static(path.join(__dirname, "public")));
+
 io.on("connection", (socket) => {
-  console.log("User connected");
+  console.log("user connected");
 
   socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+    io.emit("chat message", msg); // يبث للجميع
   });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("server running on port", PORT);
 });
