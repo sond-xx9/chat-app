c2942c7f543842c16ed70c180216b66635d06fe8
sond-xx9 <sondwe8@gmail.com>
2026-01-14 23:58:18 +0300

    Refactor server.js by removing console logs
    Removed console logs for user connection and disconnection.

--- server.js
+++ server.js
@@ -7,22 +7,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// تقديم ملفات public
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-  console.log("user connected");
-
   socket.on("chat message", (msg) => {
-    io.emit("chat message", msg); // يبث للجميع
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+    io.emit("chat message", msg);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("server running on port", PORT);
-});
+server.listen(PORT);
